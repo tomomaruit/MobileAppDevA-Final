29e677a0195fa7761f6074c6db79746d440eabc7
tomoya taniguchi <231205113@ccmailg.meijo-u.ac.jp>
2024-09-06 12:25:35 +0900

    onClickListenerの処理の書き途中

--- app/src/main/java/jp/ac/meijou/android/mobileappdeva_final/Register.java
+++ app/src/main/java/jp/ac/meijou/android/mobileappdeva_final/Register.java
@@ -9,13 +9,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.mobileappdeva_final.databinding.ActivityRegisterBinding;
+
 public class Register extends AppCompatActivity {
+
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonRegister.setOnClickListener(view -> {
+
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
